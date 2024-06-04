80628d8fc922d59dcff197fecc802b6b2b55c4b0
Onurcan Kurt <onurcankurt96@gmail.com>
2024-06-04 15:23:41 +0300

    Initial Commit

---
+++ YemekSiparisUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YemekSiparisUygulamasi
+//
+//  Created by onur on 4.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
